c7b9303222edf25fedb2b4d8d37284342eeaeabd
WenJun <nczywq@gmail.com>
2020-09-03 14:36:48 +0800

    four

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -3,10 +3,11 @@
 
 #include "pch.h"
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    cout << "Hello World!\n"; 
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
